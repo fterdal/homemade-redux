f2ee427523a85cf19d2cdd74aa0303c3cccce974
Finn <finnterdal@gmail.com>
2019-09-14 15:35:27 -0500

    can subscribe and unsubscribe

--- freeducks.js
+++ freeducks.js
@@ -1,16 +1,29 @@
 exports.createStore = reducer => {
   let state = reducer(undefined, { type: '@@INIT' })
-  const subscribers = []
+  let subscribers = []
+
+  const _nextId = () =>
+    1 +
+    subscribers
+      .map(subscriber => subscriber.id)
+      .reduce((highestId, currentId) => {
+        return currentId > highestId ? currentId : highestId
+      }, 0)
 
   const dispatch = action => {
     state = reducer(state, action)
-    subscribers.forEach(subscribeFn => {
+    subscribers.forEach(({ subscribeFn }) => {
       subscribeFn(state)
     })
   }
   const getState = () => state
-  const subscribe = (subscribeFn) => {
-    subscribers.push(subscribeFn)
+  const subscribe = subscribeFn => {
+    const id = _nextId()
+    subscribers.push({ id, subscribeFn })
+    const unsubscribe = () => {
+      subscribers = subscribers.filter(subscriber => subscriber.id !== id)
+    }
+    return unsubscribe
   }
   return {
     dispatch,
